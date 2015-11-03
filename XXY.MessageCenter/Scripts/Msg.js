0adbf41fed093b1749f7a4cc9580b0ba2108509f
gruan01 <gruan@asnum.com>
2015-11-03 11:33:18 +0800

    SignalR

--- XXY.MessageCenter/Scripts/Msg.js
+++ XXY.MessageCenter/Scripts/Msg.js
@@ -2,7 +2,17 @@
 
 var hub = $.connection.messageHub;
 hub.client.NewMsg = function (msg) {
+    $("#msgMenu #msgLabel").text("New").parent().css("color", "orangeRed");
+    $("<li class='new'><a href='#'>" + msg.Subject + "</li>").insertBefore("#msgMenu ul li:first");
+    $("#msgMenu .divider").removeClass("hidden");
 
+    var news = $("#msgMenu .new");
+    if (news.length > 5) {
+        var m;
+        for (var i = 5; m = news[i]; i++) {
+            $(m).remove();
+        }
+    }
 };
 
 $.connection.hub.start();
