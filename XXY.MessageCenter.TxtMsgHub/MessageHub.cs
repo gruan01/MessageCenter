c75f8e31fcc79bfb08d887ce7c47f090e808d5bb
gruan01 <gruan@asnum.com>
2015-11-03 10:15:18 +0800

    SignalR Missing Files

---
+++ XXY.MessageCenter.TxtMsgHub/MessageHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XXY.MessageCenter.DbEntity;
+
+namespace XXY.MessageCenter.TxtMsgHub {
+
+    public class MessageHub : Hub {
+
+        public void NewMsg(TxtMessage msg) {
+            
+        }
+
+    }
+
+}
